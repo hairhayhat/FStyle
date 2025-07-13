fc569b6e8648211f2c861c911376908c491a567d
hairhayhat <hairhayhat@gmail.com>
2025-07-13 12:20:25 +0700

    Thêm giao diện admin

--- laravel/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/hello', function () {
-    return response()->json(['message' => ' Hello from laravel API']);
-});
--- react-frontend/src/types/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  id: number;
-  username: string;
-  email: string;
-  fullName: string;
-  phone: string;
-  address: string;
-  avatar?: string;
-}
