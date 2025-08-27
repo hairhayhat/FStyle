48704acaf6e70f399e8eddbab5bf2d5859d36e3c
unknown <trungkienn2005@gmail.com>
2025-08-27 21:10:33 +0700

    committam

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $newProducts = Product::with(['category', 'variants', 'galleries'])
+        $newProducts = Product::with(['category', 'variants', 'galleries', 'activeComments'])
             ->orderBy('created_at', 'desc')
             ->take(5)
             ->get();
@@ -28,6 +28,7 @@ class HomeController extends Controller
         }
         return view('client.welcome', compact('newProducts', 'favoriteProductIds', 'categories', 'products'));
     }
+
 
     public function show($slug)
     {
