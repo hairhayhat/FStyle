1b60ec11103cb9b398fb81aacbb77560c51d5462
TrungHieu0703 <hieuhttph38450@fpt.edu.vn>
2025-09-18 21:13:42 +0700

    sua thong ke

--- database/migrations/2025_09_08_214430_update_status_enum_in_orders_table.php
+++ database/migrations/2025_09_08_214430_update_status_enum_in_orders_table.php
@@ -2,34 +2,38 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     public function up(): void
     {
-        // Thay đổi cột enum, thêm 2 trạng thái mới
-        DB::statement("ALTER TABLE orders MODIFY status ENUM(
-            'pending',
-            'confirmed',
-            'packaging',
-            'shipped',
-            'delivered',
-            'rated',
-            'cancelled',
-            'returned'
-        ) NOT NULL DEFAULT 'pending'");
+      
+        if (DB::connection()->getDriverName() !== 'sqlite') {
+            DB::statement("ALTER TABLE orders MODIFY status ENUM(
+                'pending',
+                'confirmed',
+                'packaging',
+                'shipped',
+                'delivered',
+                'rated',
+                'cancelled',
+                'returned'
+            ) NOT NULL DEFAULT 'pending'");
+        }
     }
 
     public function down(): void
     {
-        // Rollback về enum cũ
-        DB::statement("ALTER TABLE orders MODIFY status ENUM(
-            'pending',
-            'confirmed',
-            'packaging',
-            'shipped',
-            'delivered',
-            'cancelled',
-            'returned'
-        ) NOT NULL DEFAULT 'pending'");
+        if (DB::connection()->getDriverName() !== 'sqlite') {
+            DB::statement("ALTER TABLE orders MODIFY status ENUM(
+                'pending',
+                'confirmed',
+                'packaging',
+                'shipped',
+                'delivered',
+                'cancelled',
+                'returned'
+            ) NOT NULL DEFAULT 'pending'");
+        }
     }
 };
